53070d91e8645bbf113668e07fbb826be6561297
lancer <jbkhanhtran@gmail.com>
2022-10-15 10:11:47 +0700

    thêm database

---
+++ ProductQualityManager/ProductQualityManager/Models/DataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductQualityManager.Models
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null) _ins = new DataProvider();
+                return _ins;
+            }
+            set => _ins = value;
+        }
+        public BookstoreManagerEntities DB { get; set; }
+        private DataProvider()
+        {
+            DB = new BookstoreManagerEntities();
+
+        }
+    }
+
+}
