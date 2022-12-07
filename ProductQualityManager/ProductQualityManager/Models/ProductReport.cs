6ecff0e11c08610ee4c74e881401334558b38fb4
20520846 <90497301+20520846@users.noreply.github.com>
2022-12-07 20:56:22 +0700

    report commit

---
+++ ProductQualityManager/ProductQualityManager/Models/ProductReport.cs
@@ -0,0 +1,26 @@
+﻿using ProductQualityManager.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductQualityManager.Models
+{
+    public class ProductReport
+    {
+        public Nullable<int> MaSanPham { get; set; }
+        public Nullable<int> MaCoSo { get; set; }
+        public string TenSanPham { get; set; }
+        public string TenCoSo { get; set; }
+        public string TinhTrang { get; set; }
+        public ProductReport(SANPHAM SanPham)
+        {
+            this.MaSanPham = SanPham.MaSanPham;
+            this.MaCoSo = SanPham.MaCoSo;
+            this.TenSanPham = SanPham.TenSanPham;
+            this.TinhTrang = SanPham.TinhTrang;
+        }
+        public ProductReport() { }
+    }
+}
---
+++ ProductQualityManager/ProductQualityManager/ViewModels/ReportVM/ReportViewModel.cs
@@ -0,0 +1,64 @@
+﻿using ProductQualityManager.Models;
+using ProductQualityManager.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductQualityManager.ViewModels.ReportVM
+{
+    public class ReportViewModel : BaseViewModel
+    {
+        private ObservableCollection<ProductReport> _reportList;
+        public ObservableCollection<ProductReport> ReportList { get { return _reportList; } set { _reportList = value; OnPropertyChanged(nameof(ReportList)); } }
+        public ReportViewModel()
+        {
+            ReportList = new ObservableCollection<ProductReport>();
+            LoadReportList();
+        }
+        public ObservableCollection<ProductReport> GetReportFromList(List<SANPHAM> reportList)
+        {
+            ObservableCollection<ProductReport> list = new ObservableCollection<ProductReport>();
+
+            for (int i = 0; i < reportList.Count; i++)
+            {
+                if (reportList[i].TinhTrang == "Cấm" || reportList[i].TinhTrang == "Hết hạn đăng ký")
+                {
+                    ProductReport newReport = new ProductReport();
+                    newReport.MaSanPham = reportList[i].MaSanPham;
+                    newReport.TenSanPham = reportList[i].TenSanPham;
+                    newReport.MaCoSo = reportList[i].MaCoSo;
+                    newReport.TenCoSo = FindFacilityName((int)reportList[i].MaCoSo);
+                    newReport.TinhTrang = reportList[i].TinhTrang;
+
+                    list.Add(newReport);
+                }    
+            }
+
+            return list;
+        }
+        public void LoadReportList()
+        {
+            List<SANPHAM> listReport = DataProvider.Ins.DB.SANPHAMs.ToList();
+            ReportList = GetReportFromList(listReport);
+        }
+        public string FindFacilityName(int maCoSo)
+        {
+            List<SANPHAM> listProduct = DataProvider.Ins.DB.SANPHAMs.ToList();
+            List<COSOSANXUAT> listFacility = DataProvider.Ins.DB.COSOSANXUATs.ToList();
+            string tenCoSo = "";
+            for (int i = 0; i < listFacility.Count(); i++)
+            {
+                if (maCoSo == listFacility[i].MaCoSo)
+                {
+                    tenCoSo = (string)listFacility[i].TenCoSo;
+                    break;
+                }
+            }
+
+            return tenCoSo;
+        }
+    }
+}
